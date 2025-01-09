f46c4d666cc61c7398b78024317439a72d36ac0d
Xiang Liu <kakerucode@gmail.com>
2025-01-09 11:27:59 +0800

    Initial Commit

---
+++ PostRequestDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PostRequestDemo
+//
+//  Created by 刘翔 on 2025/1/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PostRequestDemo/PostRequestDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostRequestDemoApp.swift
+//  PostRequestDemo
+//
+//  Created by 刘翔 on 2025/1/9.
+//
+
+import SwiftUI
+
+@main
+struct PostRequestDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
